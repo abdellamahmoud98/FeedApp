6aaaa4b4f95c67de8bab205a3752ea2202527c8a
your-name <abdellamahmoud98@gmail.com>
2024-11-06 15:49:22 -0500

    7.12: Feed App - User Sign Up - UserService completed

--- src/main/java/com/bptn/feedapp/service/UserService.java
+++ src/main/java/com/bptn/feedapp/service/UserService.java
@@ -4,7 +4,8 @@ import java.util.Optional;
 import com.bptn.feedapp.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
-
+import java.sql.Timestamp;
+import java.time.Instant;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.bptn.feedapp.jdbc.UserBean;
@@ -35,4 +36,16 @@ public class UserService {
 	public void createUser(User user) {
 		this.userRepository.save(user);
 		}
+	
+	
+	public User signup(User user){
+		user.setUsername(user.getUsername().toLowerCase());
+		user.setEmailId(user.getEmailId().toLowerCase());
+		
+		user.setEmailVerified(false);
+		user.setCreatedOn(Timestamp.from(Instant.now()));
+		this.userRepository.save(user);
+		return user;
+	}
+	
 }
