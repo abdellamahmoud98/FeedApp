0ca0e05d435c4ab992c23f09f3368a8c239f5f4a
your-name <abdellamahmoud98@gmail.com>
2024-11-07 14:48:45 -0500

    7.16: Feed App - User Sign Up - EmailService step completed

--- src/main/java/com/bptn/feedapp/FeedAppApplication.java
+++ src/main/java/com/bptn/feedapp/FeedAppApplication.java
@@ -2,12 +2,15 @@ package com.bptn.feedapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 public class FeedAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FeedAppApplication.class, args);
+		
 	}
 
 }
