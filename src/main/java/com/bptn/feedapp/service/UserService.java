93b42a6b6ad529482a642292797531e187ba28a7
your-name <abdellamahmoud98@gmail.com>
2024-11-11 09:41:44 -0500

    7.29: Feed App - Sign up - UserService completed

--- src/main/java/com/bptn/feedapp/service/UserService.java
+++ src/main/java/com/bptn/feedapp/service/UserService.java
@@ -8,42 +8,66 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.bptn.feedapp.jpa.User;
 import com.bptn.feedapp.repository.UserRepository;
+import com.bptn.feedapp.exception.domain.EmailExistException;
+import com.bptn.feedapp.exception.domain.UsernameExistException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Service
 public class UserService {
-	
-	
+
+
 	@Autowired
 	UserRepository userRepository;
-	
+
 	@Autowired
 	EmailService emailService;
-	
-	
+
+	@Autowired
+	PasswordEncoder passwordEncoder;
+
 	public List<User> listUsers() {
 		return this.userRepository.findAll();
-		
+
 	}
-	
-	
+
+
 	public Optional<User> findUserByUsername(String username) {
 		return this.userRepository.findByUsername(username);
-		
+
 	}
-	
-	
+
+
 	public void createUser(User user) {
 		this.userRepository.save(user);
 	}
-	
-	public User signup(User user){
-		user.setUsername(user.getUsername().toLowerCase());
-		user.setEmailId(user.getEmailId().toLowerCase());
-		user.setEmailVerified(false);
-		user.setCreatedOn(Timestamp.from(Instant.now()));
-		this.userRepository.save(user);
-		this.emailService.sendVerificationEmail(user);
-		return user;
-	}
-	
+	public User signup(User user) {
+
+		 user.setUsername(user.getUsername().toLowerCase());
+		 user.setEmailId(user.getEmailId().toLowerCase());
+
+		 this.validateUsernameAndEmail(user.getUsername(), user.getEmailId());
+
+		 user.setEmailVerified(false);
+		 user.setCreatedOn(Timestamp.from(Instant.now()));
+
+		    this.userRepository.save(user);
+
+		 this.emailService.sendVerificationEmail(user);
+
+		 return user;
+		}
+
+
+	private void validateUsernameAndEmail(String username, String emailId) {
+
+        this.userRepository.findByUsername(username).ifPresent(u -> {
+            throw new UsernameExistException(String.format("Username already exists, %s", u.getUsername()));
+        });
+
+        this.userRepository.findByEmailId(emailId).ifPresent(u -> {
+            throw new EmailExistException(String.format("Email already exists, %s", u.getEmailId()));
+        });
+
+}
+
 }
