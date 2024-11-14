3c3f31ee049e8d25186e9a881b3eac1441b59e29
your-name <abdellamahmoud98@gmail.com>
2024-11-14 17:10:45 -0500

    8.32: Feed App - User Sign In Test - UserControllerTest completed

--- src/test/java/com/bptn/feedapp/FeedAppApplicationTests.java
+++ src/test/java/com/bptn/feedapp/FeedAppApplicationTests.java
@@ -1,25 +1,18 @@
 package com.bptn.feedapp;
-
-import org.junit.jupiter.api.Test;
 import org.springframework.test.context.ActiveProfiles;
-
-import org.springframework.boot.test.context.SpringBootTest;
-
 import org.junit.jupiter.api.DisplayName;
-
+import org.junit.jupiter.api.Test;
 import org.h2.tools.Server;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-
-
 @SpringBootTest
 @ActiveProfiles("test")
 class FeedAppApplicationTests {
 
-
- @DisplayName("Demo Test")
- @Test
- void contextLoads() {
- }
- @MockBean
- Server server;
+	@DisplayName("Demo Test")
+	@Test
+	void contextLoads() {
+	}
+	@MockBean
+	Server server;
 }
