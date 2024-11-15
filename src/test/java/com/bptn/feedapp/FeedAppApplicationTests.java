5e16f9f82cacf6ef87baa106a6bb1610b52fecc7
your-name <abdellamahmoud98@gmail.com>
2024-11-14 19:33:07 -0500

    latest update

--- src/test/java/com/bptn/feedapp/FeedAppApplicationTests.java
+++ src/test/java/com/bptn/feedapp/FeedAppApplicationTests.java
@@ -1,18 +1,19 @@
 package com.bptn.feedapp;
-import org.springframework.test.context.ActiveProfiles;
-import org.junit.jupiter.api.DisplayName;
+
 import org.junit.jupiter.api.Test;
-import org.h2.tools.Server;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.h2.tools.Server;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class FeedAppApplicationTests {
-	@DisplayName("Demo Test")
+
+	@MockBean
+	Server server;
+
 	@Test
 	void contextLoads() {
 	}
-	@MockBean
-	Server server;
 }
